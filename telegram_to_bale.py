bb3638701ea425f21e8bd32cced3575985f40cbb
hhabibi1014-beep <h.habibi1014@gmail.com>
2026-05-12 11:52:24 +0330

    Update telegram_to_bale.py

--- telegram_to_bale.py
+++ telegram_to_bale.py
@@ -1,18 +1,16 @@
 import requests
 from telebot import TeleBot
-
 from config import TELEGRAM_BOT_TOKEN, BALE_BOT_TOKEN, BALE_CHAT_ID
 
 telegram_bot = TeleBot(TELEGRAM_BOT_TOKEN)
 
 @telegram_bot.message_handler(commands=['start'])
 def handle_start(message):
-    telegram_bot.reply_to(message, "✅ ربات فعال شد! پیام بفرستید.")
+    telegram_bot.reply_to(message, "✅ ربات فعال شد!")
 
 @telegram_bot.message_handler(func=lambda m: True)
 def handle_message(message):
     text = message.text
-    
     url = f"https://api.bale.ai/v1/bots/{BALE_BOT_TOKEN}/sendMessage"
     data = {"chat_id": BALE_CHAT_ID, "text": text}
     
