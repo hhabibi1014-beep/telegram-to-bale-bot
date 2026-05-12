e489692ba6e43ff5fa22543eed52dcf5b90d40b0
hhabibi1014-beep <h.habibi1014@gmail.com>
2026-05-12 12:46:46 +0330

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,13 @@ def test():
     from config import BALE_BOT_TOKEN
     url = f"https://api.bale.ai/v1/bots/{BALE_BOT_TOKEN}/getMe"
     response = requests.get(url)
-    return jsonify(response.json())
-
+    
+    print(f"Status: {response.status_code}")
+    print(f"Text: {response.text}")
+    
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': response.text})
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
